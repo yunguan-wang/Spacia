41b7ac1b9d75e9fee241f33f38ceba8ff5775c6d
yunguan-wang <drizzter.yw@gmail.com>
2022-02-03 16:38:29 -0600

    made wrappers for python

--- scia-mil/scia.py
+++ scia-mil/scia.py
@@ -5,13 +5,14 @@ Dependency
 ----------
 R >= 4.0.2
     Rcpp
-
+if running in biohpc, need to do module purge; module load shared first.
 Python dependencies will be handeled automatically by installing the scia package.
 
+Test:
+cd /project/shared/xiao_wang/projects/cell2cell_inter/data
+python ../code/scia-mil/scia.py mini_Counts.txt Spot_metadata.txt C_1 C_2 pathways.txt
 """
 
-from dataclasses import replace
-from itertools import count
 import os
 import sys
 import argparse
@@ -20,16 +21,15 @@ from multiprocessing import Pool
 import numpy as np
 import pandas as pd
 from scipy.spatial.distance import cdist
-import subprocess
 
-
-def scia_worker(cmd):
+def scia_worker(scia_job_params):
     """
-    # TODO: Fillme
+    worker function for multiprocessing.
     """
-    # NOTE: fill code
-    pass
-    # os.system('rm -r output_fn') # Get rid of output.
+    scia_job_inputs = scia_job_params.split(' ')[2:5]
+    os.system('Rscript ' + scia_job_params)
+    # remove temp job input files
+    os.system('rm -f {}'.format(' '.join(scia_job_inputs)))
     return
 
 def calculate_neighbor_radius(
@@ -79,23 +79,40 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "input_path", type=str, 
-        help="Input folder containing all necessary files."
+        "counts",
+        help="Path for gene expression data, spots by genes, normalized. \
+            TXT format",
     )
 
     parser.add_argument(
-        "output_path", type=str, help="Output path")
-
-    parser.add_argument(
-        "--counts",
-        "-su",
-        help="Gene expression data, spots by genes. TXT format",
+        "spot_meta",
+        help="Path for spot positional information, spots by features. TXT format. \
+            must have 'X', 'Y', and 'cluster' columns",
     )
 
     parser.add_argument(
-        "--spot_meta",
-        "-s",
-        help="spot positional information, spots by features. TXT format",
+        "receiver_cluster", type=str,
+        help="Name of receiver cluster, must be in spot_metadata."
+    )
+
+    parser.add_argument(
+        "sender_cluster", type=str,
+        help="Name of sender cluster, must be in spot_metadata."
+    )
+    
+    parser.add_argument(
+        "pathway_lib", type=str,
+        help="Filename of the pathways to be evaluated. TXT format, two columns \
+            receiver gene/pathway, and sender gene/pathway"
+        # TODO: implement pathway level lib
+    )
+    
+    parser.add_argument(
+        "--output_path", 
+        "-o",
+        type=str,
+        default = 'scia',
+        help="Output path"
     )
 
     parser.add_argument(
@@ -106,44 +123,46 @@ if __name__ == "__main__":
         help= "Expected number of nearest neighbors for most of the cells. The \
         exact number could vary from cell to cell.",
     )
-    # parser.add_argument(
-    #     "--scia_R",
-    #     "-r",
-    #     default=0.08,
-    #     type=float,
-    #     help="Spot neighborhood radius ratio, 0-1, \
-    #         radius=R*min(xmax-xmin,ymax-ymin).",
-    # )
 
-# Setting up
-####
-    input_path = '/project/shared/xiao_wang/projects/cell2cell_inter/data'
-    output_path = '/project/shared/xiao_wang/projects/cell2cell_inter/data'
-    pathway_lib = 'pathways.txt'
-    n_neighbors = 10
-    counts = 'Counts.txt'
-    spot_meta = 'Spot_metadata.txt'
-####
-    # args = parser.parse_args()
-    # input_path = args.input_path
-    # output_path = args.output_path
-    # counts = args.counts
-    # spot_meta = args.spot_meta
+    parser.add_argument(
+        "--mcmc_params",
+        "-m",
+        type = str,
+        default = '8000,4000,10,1',
+        help= "MCMC parameters, four values packed here are {ntotal,nwarm,nthin,nchain}"
+    )
 
-    counts = pd.read_csv(
-        os.path.join(input_path, counts), index_col=0, sep='\t'
-    )
-    # TODO: add count normalizations.
-    spot_meta = pd.read_csv(
-        os.path.join(input_path, spot_meta), index_col=0, sep='\t'
-    )
-    pathway_lib = pd.read_csv(
-        os.path.join(input_path, pathway_lib), index_col=0, sep='\t'
-    )
+
+######## Setting up ########
+
+    # Debug params
+    # args = parser.parse_args(
+    #     [
+    #     '/project/shared/xiao_wang/projects/cell2cell_inter/data/Counts.txt',
+    #     '/project/shared/xiao_wang/projects/cell2cell_inter/data/Spot_metadata.txt',
+    #     'C_1',
+    #     'C_2',
+    #     '/project/shared/xiao_wang/projects/cell2cell_inter/data/pathways.txt']
+    #     )
+
+    args = parser.parse_args()
+    counts = args.counts
+    spot_meta = args.spot_meta
+    receiver_cluster = args.receiver_cluster
+    sender_cluster = args.sender_cluster
+    pathway_lib = args.pathway_lib
+    output_path = args.output_path
+    n_neighbors = args.n_neighbors
+    mcmc_params = args.mcmc_params.replace(',', ' ')
+
+    counts = pd.read_csv(counts, index_col=0, sep='\t')
+    # counts = counts.apply(lambda x: 1e6*x/x.sum(), axis=1)
+    spot_meta = pd.read_csv(spot_meta, index_col=0, sep='\t')
+    pathway_lib = pd.read_csv(pathway_lib, index_col=0, sep='\t')
     # getting script path for supporting codes.
     scia_path = os.path.abspath(__file__)
     scia_path = "/".join(scia_path.split("/")[:-1]) + '/scia'
-    # scia_script = os.path.join(scia_path, "denoise.R")
+    scia_script = os.path.join(scia_path, "SCIA_job.R")
 
     if not os.path.exists(output_path):
         os.makedirs(output_path)
@@ -166,25 +185,36 @@ if __name__ == "__main__":
     sl = StreamToLogger(stderr_logger, logging.ERROR)
     sys.stderr = sl
 
-####
-    neighbor_r = calculate_neighbor_radius(spot_meta)
-    receivers = np.random.choice(counts.index, 100, replace=False)
-    dist_matrix = cdist(spot_meta,spot_meta)
+######## Preparing scia jobs ########
+    neighbor_r = calculate_neighbor_radius(
+        spot_meta.iloc[:,:2], target_n_neighbors=n_neighbors)
+    print(
+        'Maximal distance for {} expected neighbors is {:.2f}'.format(
+            n_neighbors, neighbor_r
+        ))
+    receivers = spot_meta[spot_meta.cluster == receiver_cluster].index
+    senders = spot_meta[spot_meta.cluster == sender_cluster].index
+    dist_matrix = cdist(spot_meta.iloc[:,:2],spot_meta.iloc[:,:2])
     np.fill_diagonal(dist_matrix, np.inf)
     dist_matrix = pd.DataFrame(
         dist_matrix,
         index = spot_meta.index, columns = spot_meta.index)
-    senders = dist_matrix.loc[receivers]<=neighbor_r
+    senders = dist_matrix.loc[receivers, senders]<=neighbor_r
     senders = senders[senders.any(axis=1)]
     senders = senders.apply(lambda x: x[x].index.tolist(), axis=1)
     # Define the matrix shared by all child R processes
     sender_cells = np.unique(senders.sum())
-
     receiver_genes = pathway_lib.index.unique()
-    for receiver_gene in receiver_genes:
+    print('Total number of receiver cells: {}'.format(senders.shape[0]))
+    print('Total number of sender cells: {}'.format(len(sender_cells)))
+    # generate all scia jobs
+    scia_r_jobs_list = []
+    for _, receiver_gene in enumerate(receiver_genes):
+        print('Processing {}'.format(receiver_gene))
         sender_genes = pathway_lib.loc[receiver_gene].iloc[:,0].unique()
         sender_cts = counts.loc[sender_cells, sender_genes].round(2)
         sender_dist = dist_matrix.loc[receivers, sender_cells]
+        sender_dist = (sender_dist / neighbor_r).round(2)
         recivers_mtx = pd.DataFrame(
             senders.apply(lambda x: ','.join(x))).rename_axis(None)
         recivers_mtx.columns = ['senders']
@@ -192,11 +222,27 @@ if __name__ == "__main__":
         # NOTE: place holder rule for dichtomizing receiver gene exp
         exp_receiver = 0 + (exp_receiver > exp_receiver.median())
         recivers_mtx['exp_receiver'] = exp_receiver
+        job_id = '_'.join(['scia_job', receiver_gene])
         # Write out R wrapper data
+        scia_job_inputs = []
         for df, f_name in zip(
             [sender_cts, sender_dist, recivers_mtx],
-            ['job_cts.txt', 'job_dist.txt', 'job_receiver.txt']):
+            ['_cts.txt', '_dist.txt', '_receiver.txt']):
             df.to_csv(
-                os.path.join(output_path, f_name), sep='\t')
-        # TODO: R job submitting, results collection and clean up
-
+                os.path.join(output_path, job_id+f_name), sep='\t')
+            scia_job_inputs.append(
+                os.path.abspath(output_path) + '/' + job_id+f_name
+            )
+        scia_job_params = ' '.join([
+            scia_script,
+            scia_script.replace('SCIA_job.R',''),
+            ' '.join(scia_job_inputs),
+            job_id,
+            mcmc_params,
+            output_path + '/'
+            ]
+            )
+        scia_r_jobs_list.append(scia_job_params)
+    # run all jobs using 16 processes
+    with Pool(16) as p:
+        _ = p.map(scia_worker, scia_r_jobs_list)
--- scia-mil/scia/SCIA_job.R
+++ scia-mil/scia/SCIA_job.R
@@ -1,14 +1,42 @@
 # This is a wrapper script for MIL_wrapper.R, serving as the interface between
-# python master script and the MIL model.
+# python master script and the MIL model. The only usage for this script so far 
+# is to be called by the python wrapper.
 
 library(Rcpp)
 
 # Debug data
-setwd('E:/projects/cell2cell_inter/data/')
-scia_path = '../code/scia-mil/scia/'
-job_cts = 'job_cts.txt'
-job_dist = 'job_dist.txt'
-job_receiver = 'job_receiver.txt'
+# setwd('E:/projects/cell2cell_inter/data/')
+# scia_path = '../code/scia-mil/scia/'
+# job_cts = 'job_cts.txt'
+# job_dist = 'job_dist.txt'
+# job_receiver = 'job_receiver.txt'
+
+# Dummy test data
+# exp_receiver=c(1,1,0,0)
+# pos_sender=list(
+#  c(1,4,3),
+#  c(1),
+#  c(1,5,2,3,6),
+#  c(3)
+# )
+# 
+# # instance features, expression of genes/pathways in the sending cells
+# # rows are genes/pathways, cols are instances
+# exp_sender=list(
+#  matrix(runif(3*3),ncol=3),
+#  matrix(runif(1*3),ncol=3),
+#  matrix(runif(5*3),ncol=3),
+#  matrix(runif(1*3),ncol=3)
+# )
+# 
+# # MCMC parameters
+# ntotal=100
+# nwarm=10
+# nthin=10
+# nchain=1
+# 
+# # cutoffs on the pi_hats
+# thetas=c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)
 
 ######## Setting up ########
 
@@ -17,16 +45,51 @@ scia_path = args[1]
 job_cts = args[2]
 job_dist = args[3]
 job_receiver = args[4]
-
+job_id = args[5]
+ntotal = as.integer(args[6])
+nwarm= as.integer(args[7])
+nthin= as.integer(args[8])
+nchain= as.integer(args[9])
+output_path = args[10] # output path need to have '/' at the end
+thetas = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)
+# redirect logs
+sink(file = paste(output_path, job_id, '_log.txt', sep=''))
 #########  source codes  #################
 sourceCpp(paste(scia_path,"Fun_MICProB_C2Cinter.cpp", sep=''))
 source(paste(scia_path,'MICProB_MIL_C2Cinter.R', sep=''))
 source(paste(scia_path,'MIL_wrapper.R', sep=''))
 
 ######## format input into proper formats ########
-job_cts = read.table(job_cts)
-job_dist = read.table(job_dist, header=T, row.names = 1)
-job_receiver = read.table(job_receiver, header=T, row.names = 1)
+job_cts = read.table(job_cts,stringsAsFactors = F)
+job_dist = read.table(job_dist, header=T, row.names = 1,check.names = F,stringsAsFactors = F)
+job_receiver = read.table(job_receiver, header=T, row.names = 1,stringsAsFactors = F)
 
+# Construct the receiver vector
 exp_receiver = job_receiver$exp_receiver
 
+# construct the sender positions
+job_receiver_list = as.list(job_receiver)$senders
+pos_sender = sapply(
+  1:dim(job_receiver)[1],
+  function (i) unname(
+    unlist(
+      job_dist[i,strsplit(job_receiver[i,1],',')[[1]]])
+  ))
+
+# construct the sender expression
+exp_sender = lapply(
+  job_receiver_list,
+  function (x) unname(data.matrix((job_cts[strsplit(x,',')[[1]],]), rownames.force = F)))
+
+res = MIL_C2Cinter(exp_receiver, pos_sender, exp_sender, ntotal, nwarm, nthin, nchain, thetas)
+
+# save job result to disk
+for (n in names(res)) {
+    if (n == 'FDRs') {
+        fdr = res$FDRs
+        fdr[is.na(fdr)] = 1
+        write.table(fdr, paste(output_path, job_id,'_',n,'.txt', sep=''), sep='\t')
+    } else {
+        write.table(res[n], paste(output_path, job_id,'_',n,'.txt', sep=''), sep='\t')
+    }
+}
