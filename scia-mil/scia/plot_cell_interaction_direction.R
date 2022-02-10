5ff37dfaebcd649fbd327e432a606449479253be
zhushijia <shijiazhu@shijiazhus-macbook-pro.local>
2022-02-10 09:55:53 -0600

    update two plot function

---
+++ scia-mil/scia/plot_cell_interaction_direction.R
@@ -0,0 +1,133 @@
+#' plot_cell_connection
+#'
+#' @param image_path a character value indicating the image path
+#' @param coordinates a data.frame for cell coordinates, where each row represents a cell, with pixel_x and pixel_y indicating the coordinates
+#' @param interaction a data frame for cell-cell interaction
+#' @param xoff a numeric value indicating the offset on x-axis relative to the original image
+#' @param yoff a numeric value indicating the offset on y-axis relative to the original image 
+#' @param x_scale a numeric value indicating the augmentation scale value relative to original image
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+#' # toy example
+#' 
+#' image_path = "/archive/SCCC/Hoshida_lab/shared/fastq/SpatialTranscriptome/10X_public_dataset/MouseLiverNatComms/sample_1/img/scaled_0.1/CN73_Liver_HE_C1_0.1.jpg"
+#' # im <- readImage(image_path)
+#' coordinates = read.delim("/archive/SCCC/Hoshida_lab/shared/fastq/SpatialTranscriptome/10X_public_dataset/MouseLiverNatComms/sample_1/spots/spot_ST_CN73_Liver_C1_0.1.tsv.gz", sep="\t", header=T)
+#' rownames(coordinates) = with(coordinates, paste(x,y,sep="x"))
+#' 
+#' # estimate the spot radius
+#' spot_radius <- calculate_spot_radius(coordinates, fct)
+#' 
+#' # simulate the interactive cells using the adjacent spots
+#' dist <- calculate_cell_dist(coordinates, 4*spot_radius)
+#' set.seed(123)
+#' dist$type = sample(1:3,nrow(dist),replace=T)
+#' dist = subset(dist, j>i)
+#' interaction = with(dist, data.frame( i=i, j=j, weight=d, type=type ) )
+#' 
+#' # plot cell-cell interaction
+#' plot_cell_interaction(image_path, interaction, coordinates)
+#' 
+#' 
+plot_cell_interaction <- function( image_path, interaction, coordinates, xoff=0, yoff=0, x_scale=1 )
+{
+    library(EBImage)
+    
+    array_type <- "1k"
+    fct <- ifelse(array_type == "1k", 0.25, 50/(sqrt(2)*100))
+    
+    im <- readImage(image_path)
+    
+    display(im,method='raster')
+    plot_spot_info(coordinates, spot_cols='yellow', barcode=F, fct=fct)
+    
+    for( k in 1:nrow(interaction) )
+    {
+        i = interaction[k,1]
+        j = interaction[k,2]
+        
+        xi = ( as.numeric(coordinates[i,"pixel_x"]) - xoff )*x_scale
+        yi = ( as.numeric(coordinates[i,"pixel_y"]) - yoff )*x_scale
+        xj = ( as.numeric(coordinates[j,"pixel_x"]) - xoff )*x_scale
+        yj = ( as.numeric(coordinates[j,"pixel_y"]) - yoff )*x_scale
+        
+        wij = interaction$weight[k]
+        cij = interaction$type[k]
+        
+        arrows(xi , yi , xj, yj, col=cij, length=0.1, lwd=wij/100 )
+        
+    }
+    
+}
+
+
+calculate_spot_radius <- function(spot_coordinates, fct)
+{
+    distMat <- dist(spot_coordinates[, c("pixel_x", "pixel_y")])
+    distMat <- as.matrix(distMat)
+    diag(distMat) <- Inf
+    center_to_center_distance <- mean(apply(distMat, 2, min))
+    
+    cat("Defining spot radius...\n")
+    center_to_center_distance*fct
+    
+}
+
+plot_spot_info <- function(spot_coordinates, xoff=0, yoff=0, x_scale=1, 
+                           spot_cols='green', barcode=T, fct=0.25)
+{
+    
+    if(length(spot_cols)==1) spot_cols = rep( spot_cols, nrow(spot_coordinates) )
+    
+    plot_circle <- function(x, y, r, c) {
+        angles <- seq(0, 2*pi, length.out = 360)
+        lines(r*cos(angles) + x, r*sin(angles) + y, col=c)
+    }
+    
+    if( all(c("X","Y") %in% colnames(spot_coordinates)) )
+    {
+        spot_coordinates$pixel_x = (spot_coordinates$X-xoff)*x_scale
+        spot_coordinates$pixel_y = (spot_coordinates$Y-yoff)*x_scale
+    }
+    
+    if( all(c("imagecol","imagerow") %in% colnames(spot_coordinates)) )
+    {
+        spot_coordinates$pixel_x = (spot_coordinates$imagecol-xoff)*x_scale
+        spot_coordinates$pixel_y = (spot_coordinates$imagerow-yoff)*x_scale
+    }
+    
+    spot_radius <- calculate_spot_radius(spot_coordinates, fct)
+    
+    for(i in 1:nrow(spot_coordinates)) {
+        
+        x = as.numeric(spot_coordinates[i, "pixel_x"])
+        y = as.numeric(spot_coordinates[i, "pixel_y"])
+        r = spot_radius
+        c = spot_cols[i]
+        plot_circle(x, y, r, spot_cols)
+        
+        #text(x, y-r, as.character(spot_coordinates$barcode[i]) )
+        if(barcode) text(x, y-r, rownames(spot_coordinates)[i])
+    }
+    
+}
+
+
+calculate_cell_dist <- function(cells_on_spot, max_dist=Inf)
+{
+    do.call(rbind, lapply( 1:nrow(cells_on_spot), function(i) {   
+        cat(i,"\n")
+        d = sqrt( ( cells_on_spot[i, "pixel_x"] - cells_on_spot[, "pixel_x"])^2 + 
+                      ( cells_on_spot[i, "pixel_y"] - cells_on_spot[, "pixel_y"])^2 ) 
+        j = which(d<=max_dist)
+        j = setdiff(j,i)
+        res = NULL
+        if(length(j)>0) res = data.frame(i=i,j=j,d=d[j])
+        res
+    }))
+}
+
---
+++ scia-mil/scia/plot_interaction_prop.R
@@ -0,0 +1,113 @@
+#' plot_interaction_prop
+#'
+#' @param prop_data a data.frame representing the proportion of types of cell-cell interaction
+#' @param coordinates a data.frame representing the cell coordinates with at least two columns of "pixel_x" and "pixel_y"
+#' @param x_scale a numeric value representing the scale of image zooming
+#' @param cell_types_all a character vector representing the cell types to plot
+#' @param img_path a character value representing the image path
+#' @param scatterpie_alpha a numeric value representing the pie transparency
+#' @param pie_scale a numeric value representing the pie size
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+#' img_path = "/archive/SCCC/Hoshida_lab/shared/fastq/SpatialTranscriptome/10X_public_dataset/MouseLiverNatComms/sample_1/img/scaled_0.1/CN73_Liver_HE_C1_0.1.jpg"
+#' coordinates = read.delim("/archive/SCCC/Hoshida_lab/shared/fastq/SpatialTranscriptome/10X_public_dataset/MouseLiverNatComms/sample_1/spots/spot_ST_CN73_Liver_C1_0.1.tsv.gz", sep="\t", header=T)
+#' rownames(coordinates) = with(coordinates, paste(x,y,sep="x"))
+#' spot_radius <- calculate_spot_radius(coordinates, fct)
+#' 
+#' # simulate the interactive cells using the adjacent spots
+#' dist <- calculate_cell_dist(coordinates, max_dist=100*spot_radius)
+#' dist = subset(dist, j>i)
+#' set.seed(123)
+#' dist$type = sample( c("C1","C2","C3"),nrow(dist),replace=T)
+#' interaction = with(dist, data.frame( i=i, j=j, weight=d, type=type ) )
+#' 
+#' # caculate the proportion of cell-cell interaction types
+#' prop_data = t( apply( table( interaction$j, interaction$type ), 1, function(x) {
+#'     if(sum(x)>0) {
+#'       x/sum(x) 
+#'     } else {
+#'       rep(0,length(x))
+#'     }
+#'   } ))
+#' rownames(prop_data) = rownames(coordinates)[as.integer(rownames(prop_data))]
+#' 
+#' cell_types_all = colnames(prop_data)
+#' 
+#' # plot cell-cell interaction prop
+#' plot_interaction_prop(prop_data, coordinates, x_scale=1, cell_types_all, 
+#'                       img_path, scatterpie_alpha=1, pie_scale = 0.5)
+#' 
+plot_interaction_prop <- function( prop_data, coordinates, x_scale=1, cell_types_all, img_path=NULL, scatterpie_alpha=1, pie_scale = 0.8 )
+{
+    if(0)
+    {
+        slice <- names(se_obj@images)[1]
+        x_scale = se_obj@images[[slice]]@scale.factors$lowres
+        coordinates = data.frame(se_obj@images[[slice]]@coordinates)
+        prop_data = se_obj@meta.data
+    }
+    
+    prop_data = data.frame(barcodeID=rownames(prop_data), prop_data)
+    coordinates = data.frame(barcodeID=rownames(coordinates), coordinates)
+    spatial_coord = merge(coordinates, prop_data)
+    spatial_coord$pixel_y_scaled = spatial_coord$pixel_x*x_scale
+    spatial_coord$pixel_x_scaled = spatial_coord$pixel_y*x_scale
+    
+    if( !is.null(img_path) ) {
+        
+        
+        img_frmt <- base::tolower(stringr::str_sub(img_path, -4, -1))
+        
+        if (img_frmt %in% c(".jpg", "jpeg")) {
+            img <- jpeg::readJPEG(img_path)
+        } else if (img_frmt == ".png") {
+            img <- png::readPNG(img_path)
+        }
+        
+        img_grob <- grid::rasterGrob(img, interpolate = FALSE, width = grid::unit(1, "npc"), height = grid::unit(1, "npc"))
+        scatterpie_plt <- suppressMessages(ggplot2::ggplot() + 
+                                               ggplot2::annotation_custom(grob = img_grob, xmin = 0, xmax = ncol(img), ymin = 0, ymax = -nrow(img)) + 
+                                               scatterpie::geom_scatterpie(data = spatial_coord, 
+                                                                           ggplot2::aes(x = pixel_y_scaled, y = pixel_x_scaled), 
+                                                                           cols = cell_types_all, color = NA, 
+                                                                           alpha = scatterpie_alpha, pie_scale = pie_scale) + 
+                                               ggplot2::scale_y_reverse() + 
+                                               ggplot2::ylim(nrow(img), 0) + 
+                                               ggplot2::xlim(0, ncol(img)) + 
+                                               cowplot::theme_half_open(11, rel_small = 1) + 
+                                               ggplot2::theme_void() + 
+                                               ggplot2::coord_fixed(ratio = 1, xlim = NULL, ylim = NULL, expand = TRUE, clip = "on"))
+        scatterpie_plt
+        
+        
+            
+    } else {
+        
+        spatial_coord$pixel_x_adjusted = max(spatial_coord$pixel_x) + min(spatial_coord$pixel_x) - spatial_coord$pixel_x
+        spatial_coord$pixel_y_adjusted = max(spatial_coord$pixel_y) + min(spatial_coord$pixel_y) - spatial_coord$pixel_y
+        
+        scatterpie_plt <- suppressMessages(ggplot2::ggplot() + 
+                                               scatterpie::geom_scatterpie(data = spatial_coord, 
+                                                                           ggplot2::aes(x = pixel_x_adjusted, y = pixel_y_adjusted), 
+                                                                           cols = cell_types_all, color = NA, # if NA, then colors set by default
+                                                                           alpha = scatterpie_alpha, pie_scale = pie_scale) + 
+                                               ggplot2::scale_y_reverse() + 
+                                               ggplot2::xlim( range(spatial_coord$pixel_x_adjusted)*c(0.99, 1.01) ) + 
+                                               ggplot2::ylim( range(spatial_coord$pixel_y_adjusted)*c(0.99, 1.01) ) + 
+                                               cowplot::theme_half_open(11, rel_small = 1) + 
+                                               ggplot2::theme_void() + 
+                                               ggplot2::coord_fixed(ratio = 1, xlim = NULL, ylim = NULL, expand = TRUE, clip = "on"))
+        
+        
+    }
+    
+    scatterpie_plt
+    
+}
+
+
+
